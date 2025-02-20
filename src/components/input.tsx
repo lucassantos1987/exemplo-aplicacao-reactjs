eaccb3d6e410e7c2aa5e7f87caf216a39d94ced3
Lucas Santos <lucas.rafael.silva.santos@gmail.com>
2025-02-20 18:49:57 -0300

    sdsds

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -10,7 +10,7 @@ export function InputRoot({ error = false, ...props}: InputRootProps) {
       data-error={error}
       className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2
       focus-within:border-gray-100 
-      data-[error=true]:border-danger"
+      data-[error=true]:border-danger" 
       {...props}/>
   )
 }
