ef70a7ddc72e84f5e36620af5af60b1eef48348c
Lucas Santos <lucas.rafael.silva.santos@gmail.com>
2025-02-20 18:55:32 -0300

    Testando branch

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -31,6 +31,7 @@ interface InputFieldProps extends ComponentProps<'input'>{}
 
 export function InputField(props: InputFieldProps) {
   return (
+    
     <input className="flex-1 outline-0 placeholder-gray-400"
     placeholder="Digite seu email" 
     {...props}/>
